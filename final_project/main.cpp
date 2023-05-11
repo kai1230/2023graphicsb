bf002c9b0c527e147614198d146f924b4e200d0e
kai1230 <hsujasonkai@gmail.com>
2023-05-11 16:11:29 +0800

    week13

--- final_project/main.cpp
+++ final_project/main.cpp
@@ -1,14 +1,39 @@
 #include<stdio.h>
 #include<GL/glut.h>
+#include "glm.h"
+
+GLMmodel * head = NULL;
+GLMmodel * body = NULL;
+GLMmodel * ruparmR = NULL;
+GLMmodel * rlowarmR = NULL;
+int show[4]={1,0,0,0};
+
 float teapotX=0,teapotY=0;
 FILE * fout =NULL;
 FILE * fin=NULL;
+void keyboard(unsigned char key,int x,int y){
+    if(key=='0')show[0]=! show[0];
+    if(key=='1')show[1]=! show[1];
+    if(key=='2')show[2]=! show[2];
+    if(key=='3')show[3]=! show[3];
+    glutPostRedisplay();
+}
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    if(head==NULL){
+        head =glmReadOBJ("model/head.obj");
+        body =glmReadOBJ("model/body.obj");
+        ruparmR =glmReadOBJ("model/ruparm.obj");
+        rlowarmR =glmReadOBJ("model/rlowarm.obj");
+        ///glmUnitize(head);
+    }
     glPushMatrix();
-        glTranslatef(teapotX,teapotY,0);
-        glutSolidTeapot(0.3);
+        glScalef(0.3,0.3,0.3);
+        if (show[0])glmDraw(head,GLM_MATERIAL);
+        if (show[1])glmDraw(body,GLM_MATERIAL);
+        if (show[2])glmDraw(ruparmR,GLM_MATERIAL);
+        if (show[3])glmDraw(rlowarmR,GLM_MATERIAL);
     glPopMatrix();
     glutSwapBuffers();
 }
@@ -23,7 +48,7 @@ void mouse(int button,int state,int x,int y)
     }
     display();
 }
-void keyboard(unsigned char key,int x,int y)
+/*void keyboard(unsigned char key,int x,int y)
 {
     if(fin==NULL){
         fclose(fout);
@@ -31,7 +56,7 @@ void keyboard(unsigned char key,int x,int y)
     }
     fscanf(fin,"%f%f",&teapotX,&teapotY);
     display();
-}
+}*/
 int main(int argc,char**argv)
 {
     glutInit(&argc,argv);
